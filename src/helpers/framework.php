be47577f99dd714281c72d4b3e5f27c2ae4ab998
Harold <mail@yaolong.me>
2016-06-12 18:09:06 +0800

    init

--- src/helpers/framework.php
+++ src/helpers/framework.php
@@ -12,13 +12,3 @@ if (!function_exists("is_production")) {
         return boolval((getenv('ENV') == "production"));
     }
 }
-
-if (!function_exists("document_uri")) {
-    function document_uri()
-    {
-        if (($uri_query_mark = strpos($_SERVER['REQUEST_URI'], "?")) <= 0) {
-            return $_SERVER['REQUEST_URI'];
-        }
-        return substr($_SERVER['REQUEST_URI'], 0, $uri_query_mark);
-    }
-}
