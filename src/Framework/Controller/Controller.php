775b309def60f77531bfdad6202ac17a36cc989e
Harold <mail@yaolong.me>
2016-06-13 11:53:25 +0800

    add templationg require

---
+++ src/Framework/Controller/Controller.php
@@ -0,0 +1,36 @@
+<?php
+namespace Impress\Framework\Controller;
+
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
+
+class Controller
+{
+    private static $request;
+
+    protected function __construct()
+    {
+
+    }
+
+    protected function request(
+        array $query = array(),
+        array $request = array(),
+        array $attributes = array(),
+        array $cookies = array(),
+        array $files = array(),
+        array $server = array(),
+        $content = null
+    )
+    {
+        if (!(self::$request instanceof Request)) {
+            empty($query) && $query = $_GET;
+            empty($request) && $request = $_POST;
+            empty($cookies) && $cookies = $_COOKIE;
+            empty($files) && $files = $_FILES;
+            empty($server) && $server = $_SERVER;
+            self::$request = new Request($query, $request, $attributes, $cookies, $files, $server, $content);
+        }
+        return self::$request;
+    }
+}
