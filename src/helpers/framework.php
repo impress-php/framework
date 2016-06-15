287d154622607dc3ba2784c11470ce96f0ebde49
Harold <mail@yaolong.me>
2016-06-15 17:38:43 +0800

    add response helpers

--- src/helpers/framework.php
+++ src/helpers/framework.php
@@ -12,3 +12,33 @@ if (!function_exists("is_production")) {
         return boolval((getenv('ENV') == "production"));
     }
 }
+
+if (!function_exists("response")) {
+    function response($content = "", $statusCode = 200, array $headers = array())
+    {
+        $response = new \Symfony\Component\HttpFoundation\Response();
+        $response->setContent($content);
+        $response->setStatusCode($statusCode);
+        $response->headers->add($headers);
+        return $response;
+    }
+}
+
+if (!function_exists("view")) {
+    function view($name, array $data = array(), $statusCode = 200, array $headers = array(), $engine = \Impress\Framework\View\View::ENGINE_AUTO)
+    {
+        $content = \Impress\Framework\View\View::make($name, $data, $engine);
+        return response($content, $statusCode, $headers);
+    }
+}
+
+if (!function_exists("json")) {
+    function json(array $data, $statusCode = 200, array $headers = array(
+        "Content-Type" => "application/json"
+    ))
+    {
+        $content = json_encode($data);
+        return response($content, $statusCode, $headers);
+    }
+}
+
