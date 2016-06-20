f05e3a5b75f6426b95cef1235098ce97d9f56f60
Harold <mail@yaolong.me>
2016-06-20 23:27:15 +0800

    add file session handler

--- src/Framework/Http/Controller.php
+++ src/Framework/Http/Controller.php
@@ -1,6 +1,8 @@
 <?php
 namespace Impress\Framework\Http;
 
+use Impress\Framework\Http\Session\Session;
+
 class Controller
 {
     private static $response;
@@ -21,6 +23,34 @@ class Controller
     )
     {
         return Request::request($query, $request, $attributes, $cookies, $files, $server, $content);
+    }
+
+    public function session(
+        array $query = array(),
+        array $request = array(),
+        array $attributes = array(),
+        array $cookies = array(),
+        array $files = array(),
+        array $server = array(),
+        $content = null
+    )
+    {
+        return $this->request($query, $request, $attributes, $cookies, $files, $server, $content)->getSession();
+    }
+
+    public function session_start(
+        array $query = array(),
+        array $request = array(),
+        array $attributes = array(),
+        array $cookies = array(),
+        array $files = array(),
+        array $server = array(),
+        $content = null
+    )
+    {
+        $session = new Session(getenv("SESSION_DRIVER"));
+        $this->request($query, $request, $attributes, $cookies, $files, $server, $content)->setSession($session);
+        session_start();
     }
 
     public function clearRequest()
--- src/Framework/Http/Session/Session.php
+++ src/Framework/Http/Session/Session.php
@@ -1,35 +1,49 @@
 <?php
-namespace Impress\Framework\Http;
+namespace Impress\Framework\Http\Session;
 
+use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
 use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBagInterface;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpFoundation\Session\Session as VendorSession;
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeFileSessionHandler;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 use Symfony\Component\HttpFoundation\Session\Storage\SessionStorageInterface;
 
 class Session extends VendorSession
 {
-    public function __construct($driver, SessionStorageInterface $storage = null, AttributeBagInterface $attributes = null, FlashBagInterface $flashes = null)
+    /**
+     * Session constructor.
+     * @param String $driver
+     * @param array $options
+     * @param SessionStorageInterface|null $storage
+     * @param AttributeBagInterface|null $attributes
+     * @param FlashBagInterface|null $flashes
+     */
+    public function __construct($driver, array $options = array(), SessionStorageInterface $storage = null, AttributeBagInterface $attributes = null, FlashBagInterface $flashes = null)
     {
-        switch ($driver) {
+        switch ($driver = strtolower($driver)) {
             case 'redis':
-                break;
             case 'predis':
-                break;
             case 'memcache':
-                break;
             case 'memcached':
-                break;
             case 'mongodb':
             case 'mongo':
-                break;
             case 'pdo':
-                break;
             case 'file':
             default:
+                $handler = call_user_func([$this, "{$driver}SessionHandler"]);
                 break;
         }
-        $storage = $storage ?: new NativeSessionStorage();
+
+        $options = $options ?: config(getenv("SESSION_OPTIONS"));
+        $storage = $storage ?: new NativeSessionStorage($options, $handler);
+        $attributes = $attributes ?: new AttributeBag("_ips_attributes");
         parent::__construct($storage, $attributes, $flashes);
+    }
+
+    private function fileSessionHandler()
+    {
+        $handler = new NativeFileSessionHandler(SESSIONS_DIR);
+        return $handler;
     }
 }
