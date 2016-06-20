a2818331bfc0aef1bda9499372c966cd73fd0b38
Harold <mail@yaolong.me>
2016-06-20 18:33:53 +0800

    init RedisSessionHandlers

--- src/Framework/Http/Session/Session.php
+++ src/Framework/Http/Session/Session.php
@@ -4,19 +4,32 @@ namespace Impress\Framework\Http;
 use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBagInterface;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpFoundation\Session\Session as VendorSession;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 use Symfony\Component\HttpFoundation\Session\Storage\SessionStorageInterface;
 
 class Session extends VendorSession
 {
-    /**
-     * Constructor.
-     *
-     * @param SessionStorageInterface $storage    A SessionStorageInterface instance.
-     * @param AttributeBagInterface   $attributes An AttributeBagInterface instance, (defaults null for default AttributeBag)
-     * @param FlashBagInterface       $flashes    A FlashBagInterface instance (defaults null for default FlashBag)
-     */
-    public function __construct(SessionStorageInterface $storage = null, AttributeBagInterface $attributes = null, FlashBagInterface $flashes = null)
+    public function __construct($driver, SessionStorageInterface $storage = null, AttributeBagInterface $attributes = null, FlashBagInterface $flashes = null)
     {
+        switch ($driver) {
+            case 'redis':
+                break;
+            case 'predis':
+                break;
+            case 'memcache':
+                break;
+            case 'memcached':
+                break;
+            case 'mongodb':
+            case 'mongo':
+                break;
+            case 'pdo':
+                break;
+            case 'file':
+            default:
+                break;
+        }
+        $storage = $storage ?: new NativeSessionStorage();
         parent::__construct($storage, $attributes, $flashes);
     }
 }
